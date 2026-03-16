382a87f6f14fc44d0a9bead8924fbfdc2431c1ca
sebas3536 <delahozpablo005@gmail.com>
2026-03-16 01:09:07 -0500

    Curim V.0.0.1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api.curimbot.com',
-    dashboardRefreshInterval: 30000,
+  production: false,
+  apiUrl: 'https://api.curimbot.com',
+  dashboardRefreshInterval: 30000,
 };
-
